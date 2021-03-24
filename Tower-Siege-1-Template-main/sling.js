349d47b4d299bd6f99f4ce975d2ac05387bd4124
Yashaswi1919 <77353042+Yashaswi1919@users.noreply.github.com>
2021-03-24 02:15:26 -0700

    Add files via upload

---
+++ Tower-Siege-1-Template-main/sling.js
@@ -0,0 +1,17 @@
+class Slingshot{
+    constructor(x,y){
+    var options={
+
+    }
+    this.body=Bodies.circle(50,200,20);
+    World.add(world,polygon);
+
+    }
+    display(){
+    push();
+    imageMode(CENTER)
+    image(polygon_img,polygon.position.x,polygon.position.y,40,40);
+    pop();
+
+    }
+}
